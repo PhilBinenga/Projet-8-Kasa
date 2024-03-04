dbedeb66a5c673fcd320698c309fcd439f87294e
Phil BINENGA <114619068+PhilBinenga@users.noreply.github.com>
2024-03-04 12:58:41 +0100

    Add files via upload

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Banner from '../../components/banner/Banner.jsx'
+import Cards from '../../components/cards/Cards.jsx'
+import Container from '../../components/Container/Container.jsx'
+import bannerHome from '../../assets/images/banner-img.png'
+function Home() {
+  return (
+    <div>
+      <Banner
+      titre="Chez vous, partout et ailleurs"
+      src= {bannerHome}
+      alt="banner home"
+     />
+      <Cards />
+      <Container />
+    </div>
+  )
+}
+
+export default Home
